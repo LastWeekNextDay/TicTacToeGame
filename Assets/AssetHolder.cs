1b0bc46d420cee6caa166388a88c3c11e5a5bafa
LastWeekNextDay <56513101+LastWeekNextDay@users.noreply.github.com>
2023-10-24 16:55:20 +0300

    Changed prefab naming

--- Assets/AssetHolder.cs
+++ Assets/AssetHolder.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 public class AssetHolder : ScriptableObject
 {
-    public GameObject XPrefab = null;
-    public GameObject OPrefab = null;
-    public GameObject SlotPrefab = null;
+    public GameObject XObjPrefab = null;
+    public GameObject OObjPrefab = null;
+    public GameObject SlotObjPrefab = null;
     // Start is called before the first frame update
     void Start()
     {
-        XPrefab = Resources.Load<GameObject>("XObj");
-        OPrefab = Resources.Load<GameObject>("OObj");
-        SlotPrefab = Resources.Load<GameObject>("SlotObj");
+        XObjPrefab = Resources.Load<GameObject>("XObj");
+        OObjPrefab = Resources.Load<GameObject>("OObj");
+        SlotObjPrefab = Resources.Load<GameObject>("SlotObj");
     }
 
     // Update is called once per frame
