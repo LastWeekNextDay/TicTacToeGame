d621b8db5fa616e028e74ff4da266c464357adad
LastWeekNextDay <56513101+LastWeekNextDay@users.noreply.github.com>
2023-10-24 16:45:18 +0300

    Changed AssetHolder to ScriptableObject

--- Assets/AssetHolder.cs
+++ Assets/AssetHolder.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AssetHolder : MonoBehaviour
+public class AssetHolder : ScriptableObject
 {
     public GameObject XPrefab = null;
     public GameObject OPrefab = null;
