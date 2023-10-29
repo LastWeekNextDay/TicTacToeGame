67c77c0ed6c64fb32abf3059563bdd3c68d8e139
WiciokS <77240194+WiciokS@users.noreply.github.com>
2023-10-29 23:07:33 +0200

    Extended Menu
    Added new fields and some logic

---
+++ Assets/Scripts/UI/MenuStart.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class MenuStart : MonoBehaviour
+{
+    public GameObject mainMenu;
+    public GameObject singlePlayer;
+    public GameObject multiPlayer;
+    public GameObject options;
+    public GameObject host;
+    public GameObject join;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (mainMenu != null) mainMenu.SetActive(true);
+        if (singlePlayer != null) singlePlayer.SetActive(false);
+        if (multiPlayer != null) multiPlayer.SetActive(false);
+        if (options != null) options.SetActive(false);
+        if (host != null) host.SetActive(false);
+        if (join != null) join.SetActive(false);
+    }
+}
