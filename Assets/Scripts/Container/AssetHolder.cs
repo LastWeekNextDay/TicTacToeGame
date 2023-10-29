a5d3957479c2811754858c7ec3886bd47e7121ee
LastWeekNextDay <56513101+LastWeekNextDay@users.noreply.github.com>
2023-10-29 22:27:42 +0200

    Initial MP rework

--- Assets/Scripts/Container/AssetHolder.cs
+++ Assets/Scripts/Container/AssetHolder.cs
@@ -9,6 +9,7 @@ public class AssetHolder : MonoBehaviour
     public GameObject SlotObjPrefab = null;
     public GameObject AIPlayerObjPrefab = null;
     public GameObject HumanPlayerObjPrefab = null;
+    public GameObject NetworkManagerObjPrefab = null;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,6 +18,7 @@ public class AssetHolder : MonoBehaviour
         SlotObjPrefab = Resources.Load<GameObject>("SlotObj");
         AIPlayerObjPrefab = Resources.Load<GameObject>("AIPlayerObj");
         HumanPlayerObjPrefab = Resources.Load<GameObject>("HumanPlayerObj");
+        NetworkManagerObjPrefab = Resources.Load<GameObject>("NetworkManager");
     }
 
     // Update is called once per frame
