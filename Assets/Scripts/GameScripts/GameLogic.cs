cb7f61aa0251fbb80b6ebadb28dc2676a95ff4c8
LastWeekNextDay <56513101+LastWeekNextDay@users.noreply.github.com>
2023-11-02 22:30:13 +0200

    Update GameLogic.cs

--- Assets/Scripts/GameScripts/GameLogic.cs
+++ Assets/Scripts/GameScripts/GameLogic.cs
@@ -105,8 +105,6 @@ public class GameLogic : MonoBehaviour
         yield return NetworkManager.RoomConnectionInitialization();
         Debug.Log("Host has joined!");
         Player1 = CreatePlayer(_assetHolder.HumanPlayerMPObjPrefab);
-        PhotonNetwork.AllocateViewID(Player1.GetComponent<PhotonView>());
-        Player1.gameObject.GetComponent<PhotonView>().TransferOwnership(PhotonNetwork.LocalPlayer.ActorNumber);
         StartCoroutine(SetupMultiPlayer2());
     }
 
@@ -115,8 +113,6 @@ public class GameLogic : MonoBehaviour
         yield return NetworkManager.WaitForSecondPlayer();
         Debug.Log("Other player has joined!");
         Player2 = CreatePlayer(_assetHolder.HumanPlayerMPObjPrefab);
-        PhotonNetwork.AllocateViewID(Player2.GetComponent<PhotonView>());
-        Player2.gameObject.GetComponent<PhotonView>().TransferOwnership(NetworkManager.GetPlayer(1).ActorNumber);
         int size = -1;
         int winCon = -1;
         if (SessionInfo.Instance.MultiplayerType == "Host")
