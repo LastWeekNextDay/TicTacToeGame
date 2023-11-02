e8e6f1e1ca2157d0e139af7f80af9c77bb69d332
LastWeekNextDay <56513101+LastWeekNextDay@users.noreply.github.com>
2023-11-03 01:58:42 +0200

    Update GameLogic.cs

--- Assets/Scripts/GameScripts/GameLogic.cs
+++ Assets/Scripts/GameScripts/GameLogic.cs
@@ -67,10 +67,10 @@ public class GameLogic : MonoBehaviour
     public void ChangeTurn()
     {
         Turn = (Turn == Player1) ? Player2 : Player1;
-        //if (SessionInfo.Instance.Multiplayer)
-        //{
-        //    NetworkManager.ChangeTurnTo(Turn.GetComponent<PhotonView>().ViewID);
-        //}
+        if (SessionInfo.Instance.Multiplayer)
+        {
+            NetworkManager.ChangeTurnTo(Turn.GetComponent<PhotonView>().ViewID);
+        }
         OnChangeTurn();
     }
 
