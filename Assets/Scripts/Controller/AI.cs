0b3d50b39ac9a9b14bff4ebc21ee83ee454b5238
LastWeekNextDay <56513101+LastWeekNextDay@users.noreply.github.com>
2023-11-07 09:47:55 +0200

    def final

--- Assets/Scripts/Controller/AI.cs
+++ Assets/Scripts/Controller/AI.cs
@@ -45,7 +45,7 @@ public class AI : Player
         _justPlacedPiece = false;
         TicTacToeGrid gridBase = _gameLogic.Grid.GridBase;
         int gridSize = gridBase.Size;
-        if (gridSize < 1) { yield break; };
+        if (!_gameLogic.GameActive) { yield break; };
         Dictionary<int, List<int>> valuesXY = new Dictionary<int, List<int>>();
         for (int x = 0; x < gridSize; x++)
         {
